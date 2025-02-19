4caa99317b5d188ef58e5e1e85c5c7aec1810669
Hayden Rose <hayden4r4@gmail.com>
2025-02-18 19:34:15 -0800

    added untested arm simd

--- src/wide_32ix256.rs
+++ src/wide_32ix256.rs
@@ -1,6 +1,6 @@
 //--------------------------------------------------------------------------
 // x86/64 AVX2
-#[cfg(all(target_arch = "x86_64", feature = "simd", target_feature = "+avx2"))]
+#[cfg(all(target_arch = "x86_64", feature = "simd"))]
 pub(crate) mod avx2 {
     use core::arch::x86_64::{
         __m256i, _mm256_add_epi32, _mm256_and_si256, _mm256_or_si256, _mm256_set1_epi32,
@@ -312,12 +312,312 @@ pub(crate) mod avx2 {
         }
     }
 }
-#[cfg(all(target_arch = "x86_64", feature = "simd", target_feature = "+avx2",))]
+#[cfg(all(target_arch = "x86_64", feature = "simd"))]
 pub use avx2::Int8;
 
 //--------------------------------------------------------------------------
+// ARM NEON
+#[cfg(all(target_arch = "aarch64", feature = "simd"))]
+pub(crate) mod neon {
+    use core::arch::aarch64::{
+        uint32x4_t, vaddq_u32, vandq_u32, vdupq_n_u32, vdupq_n_f32, veorq_u32, vorrq_u32,
+        vsetq_lane_u32, vsetq_lane_f32, vshlq_n_u32, vshrq_n_u32, vsubq_u32, vsubq_f32, vmovq_n_u32,
+        vreinterpretq_f32_u32, vreinterpretq_u32_f32,
+    };
+
+    /// A packed set of 8 `u32`s.
+    ///
+    /// Addition, subtraction, and multiplication are all wrapping.
+    ///
+    /// Uses SIMD for computation on supported platforms.
+    #[derive(Debug, Copy, Clone)]
+    pub struct Int8 {
+        v: uint32x4_t,
+    }
+
+    impl Int8 {
+        #[inline(always)]
+        pub(crate) fn zero() -> Int8 {
+            Int8 {
+                v: unsafe { vmovq_n_u32(0) },
+            }
+        }
+
+        /// For testing.
+        #[allow(dead_code)]
+        fn get(self, i: usize) -> u32 {
+            let n: [u32; 4] = unsafe { core::mem::transmute(self) };
+            n[i]
+        }
+
+        /// Convert each integer to a float in [0.0, 1.0).
+        ///
+        /// Same behavior as
+        /// [`parts::u32_to_f32_norm()`](`crate::parts::u32_to_f32_norm()`),
+        /// applied to each integer individually.
+        #[inline(always)]
+        pub fn to_f32_norm(self) -> [f32; 4] {
+            let n4 = unsafe {
+                let a = vshrq_n_u32(self.v, 9);
+                let b = vorrq_u32(a, vdupq_n_u32(core::mem::transmute(0x3f800000u32)));
+                vsubq_f32(vreinterpretq_f32_u32(b), vdupq_n_f32(1.0))
+            };
+
+            unsafe { core::mem::transmute(n4) }
+        }
+
+        /// Reverse the order of the bits in each integer.
+        ///
+        /// Same behavior as `reverse_bits()` in the Rust standard
+        /// library, applied to each integer individually.
+        #[inline]
+        pub fn reverse_bits(self) -> Int8 {
+            let mut n = self.v;
+            unsafe {
+                // From http://aggregate.org/MAGIC/#Bit%20Reversal but SIMD
+                // on 8 numbers at once.
+
+                let y0 = vdupq_n_u32(core::mem::transmute(0x55555555u32));
+                n = vorrq_u32(
+                    vandq_u32(vshrq_n_u32(n, 1), y0),
+                    vshlq_n_u32(vandq_u32(n, y0), 1),
+                );
+
+                let y1 = vdupq_n_u32(core::mem::transmute(0x33333333u32));
+                n = vorrq_u32(
+                    vandq_u32(vshrq_n_u32(n, 2), y1),
+                    vshlq_n_u32(vandq_u32(n, y1), 2),
+                );
+
+                let y2 = vdupq_n_u32(core::mem::transmute(0x0f0f0f0fu32));
+                n = vorrq_u32(
+                    vandq_u32(vshrq_n_u32(n, 4), y2),
+                    vshlq_n_u32(vandq_u32(n, y2), 4),
+                );
+
+                let y3 = vdupq_n_u32(core::mem::transmute(0x00ff00ffu32));
+                n = vorrq_u32(
+                    vandq_u32(vshrq_n_u32(n, 8), y3),
+                    vshlq_n_u32(vandq_u32(n, y3), 8),
+                );
+
+                n = vorrq_u32(vshrq_n_u32(n, 16), vshlq_n_u32(n, 16));
+
+                Int8 { v: n }
+            }
+        }
+    }
+
+    impl core::ops::Mul for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn mul(self, other: Self) -> Int8 {
+            unsafe {
+                use core::arch::aarch64::vmulq_u32;
+                Int8 {
+                    v: vmulq_u32(self.v, other.v),
+                }
+            }
+        }
+    }
+
+    impl core::ops::MulAssign for Int8 {
+        #[inline(always)]
+        fn mul_assign(&mut self, other: Self) {
+            *self = *self * other;
+        }
+    }
+
+    impl core::ops::Add for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn add(self, other: Self) -> Self {
+            Int8 {
+                v: unsafe { vaddq_u32(self.v, other.v) },
+            }
+        }
+    }
+
+    impl core::ops::AddAssign for Int8 {
+        #[inline(always)]
+        fn add_assign(&mut self, other: Self) {
+            *self = *self + other;
+        }
+    }
+
+    impl core::ops::Sub for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn sub(self, other: Self) -> Self {
+            Int8 {
+                v: unsafe { vsubq_u32(self.v, other.v) },
+            }
+        }
+    }
+
+    impl core::ops::SubAssign for Int8 {
+        #[inline(always)]
+        fn sub_assign(&mut self, other: Self) {
+            *self = *self - other;
+        }
+    }
+
+    impl core::ops::BitAnd for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn bitand(self, other: Self) -> Int8 {
+            Int8 {
+                v: unsafe { vandq_u32(self.v, other.v) },
+            }
+        }
+    }
+
+    impl core::ops::BitAndAssign for Int8 {
+        #[inline(always)]
+        fn bitand_assign(&mut self, other: Self) {
+            *self = *self & other;
+        }
+    }
+
+    impl core::ops::BitOr for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn bitor(self, other: Self) -> Int8 {
+            Int8 {
+                v: unsafe { vorrq_u32(self.v, other.v) },
+            }
+        }
+    }
+
+    impl core::ops::BitOrAssign for Int8 {
+        #[inline(always)]
+        fn bitor_assign(&mut self, other: Self) {
+            *self = *self | other;
+        }
+    }
+
+    impl core::ops::BitXor for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn bitxor(self, other: Self) -> Int8 {
+            Int8 {
+                v: unsafe { veorq_u32(self.v, other.v) },
+            }
+        }
+    }
+
+    impl core::ops::BitXorAssign for Int8 {
+        #[inline(always)]
+        fn bitxor_assign(&mut self, other: Self) {
+            *self = *self ^ other;
+        }
+    }
+
+    impl core::ops::Shl<i32> for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn shl(self, other: i32) -> Int8 {
+            Int8 {
+                v: unsafe { vshlq_n_u32(self.v, other) },
+            }
+        }
+    }
+
+    impl core::ops::Shr<i32> for Int8 {
+        type Output = Self;
+        #[inline(always)]
+        fn shr(self, other: i32) -> Int8 {
+            Int8 {
+                v: unsafe { vshrq_n_u32(self.v, other) },
+            }
+        }
+    }
+
+    impl From<[u32; 4]> for Int8 {
+        #[inline(always)]
+        fn from(v: [u32; 4]) -> Self {
+            Int8 {
+                v: unsafe { core::mem::transmute(v) },
+            }
+        }
+    }
+
+    impl From<Int8> for [u32; 4] {
+        #[inline(always)]
+        fn from(i: Int8) -> [u32; 4] {
+            unsafe { core::mem::transmute(i.v) }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn from_array() {
+            let a = Int8::from([1, 2, 3, 4]);
+            assert_eq!(a.get(0), 1);
+            assert_eq!(a.get(1), 2);
+            assert_eq!(a.get(2), 3);
+            assert_eq!(a.get(3), 4);
+        }
+
+        #[test]
+        fn shr() {
+            let a = Int8::from([0xffffffff; 4]) >> 16;
+            assert_eq!(a.get(0), 0x0000ffff);
+            assert_eq!(a.get(1), 0x0000ffff);
+            assert_eq!(a.get(2), 0x0000ffff);
+            assert_eq!(a.get(3), 0x0000ffff);
+        }
+
+        #[test]
+        fn shl() {
+            let a = Int8::from([0xffffffff; 4]) << 16;
+            assert_eq!(a.get(0), 0xffff0000);
+            assert_eq!(a.get(1), 0xffff0000);
+            assert_eq!(a.get(2), 0xffff0000);
+            assert_eq!(a.get(3), 0xffff0000);
+        }
+
+        #[test]
+        fn to_f32_norm() {
+            let a = Int8::from([0x00000000; 4]);
+            let b = Int8::from([0x80000000; 4]);
+            let c = Int8::from([0xffffffff; 4]);
+
+            let a2 = a.to_f32_norm();
+            let b2 = b.to_f32_norm();
+            let c2 = c.to_f32_norm();
+
+            assert_eq!(a2, [0.0, 0.0, 0.0, 0.0]);
+            assert_eq!(b2, [0.5, 0.5, 0.5, 0.5]);
+            assert!(c2[0] > 0.99999 && c2[0] < 1.0);
+            assert!(c2[1] > 0.99999 && c2[1] < 1.0);
+            assert!(c2[2] > 0.99999 && c2[2] < 1.0);
+            assert!(c2[3] > 0.99999 && c2[3] < 1.0);
+        }
+
+        #[test]
+        fn reverse_bits() {
+            let a = 0xcde7a64e_u32;
+            let b = 0xdc69fbd9_u32;
+            let c = 0x3238fec6_u32;
+            let d = 0x1fb9ba8f_u32;
+
+            assert_eq!(Int8::from([a; 4]).reverse_bits().get(0), a.reverse_bits());
+            assert_eq!(Int8::from([b; 4]).reverse_bits().get(0), b.reverse_bits());
+            assert_eq!(Int8::from([c; 4]).reverse_bits().get(0), c.reverse_bits());
+            assert_eq!(Int8::from([d; 4]).reverse_bits().get(0), d.reverse_bits());
+        }
+    }
+}
+#[cfg(all(target_arch = "aarch64", feature = "simd"))]
+pub use neon::Int8;
+
+//--------------------------------------------------------------------------
 // Fallback
-#[cfg(not(all(target_arch = "x86_64", feature = "simd", target_feature = "+avx2",)))]
+#[cfg(not(all(target_arch = "x86_64", feature = "simd")))]
 pub(crate) mod fallback {
     /// A packed set of 16 `u32`s.
     ///
@@ -584,5 +884,5 @@ pub(crate) mod fallback {
         }
     }
 }
-#[cfg(not(all(target_arch = "x86_64", feature = "simd", target_feature = "+avx2",)))]
+#[cfg(not(all(target_arch = "x86_64", feature = "simd")))]
 pub use fallback::Int8;
